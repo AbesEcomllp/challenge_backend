d8f66434c5be4aecc430bdacc253b6b8be56607d
AbesEcomllp <abesecomllp25@gmail.com>
2025-05-17 13:00:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const app = express();
 export default app;
 
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://himalixir.com"], // Allowed origins
+    origin: ["http://localhost:5173", "https://himalixir.com", "*"], // Allowed origins
     methods: ['GET', 'POST', 'PUT'], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "x-webhook-signature"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "x-webhook-signature", "*"],
  // Allowed headers
     
 };
