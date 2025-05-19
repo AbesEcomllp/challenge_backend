cc0329d4e4c8baea7659fda78440036c84554b2f
AbesEcomllp <abesecomllp25@gmail.com>
2025-05-19 14:56:27 +0530

    Update Admin.model.js

--- Models/Admin.model.js
+++ Models/Admin.model.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const AdminSchema = new Schema(
