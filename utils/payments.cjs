9f7a1ed61a05a7ea45d2a7701a922fa1688ae9b7
bulkhead1tech <thebulkhead1@gmail.com>
2025-05-19 10:30:53 +0530

    mian

--- utils/payments.cjs
+++ utils/payments.cjs
@@ -5,9 +5,8 @@ const  Cashfree  = require('cashfree-pg');
 // Step 2: Configure Cashfree SDK with API credentials
 
 const paymentHandler = async (amount, order_id, cust_id, phone) => {
-  const id =  process.env.Client_ID;
-const secret =process.env.Client_SECRET_KEY;
-  console.log(id)
+  const id =  process.env.CLIENT_ID;
+const secret =process.env.CLIENT_SECRET_KEY;
   const options = {
     method: 'POST',
     headers: {
