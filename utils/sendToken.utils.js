9901186678492b57a43709c9263561e3f3fde289
bulkhead1tech <thebulkhead1@gmail.com>
2025-05-21 12:40:57 +0530

    path

--- utils/sendToken.utils.js
+++ utils/sendToken.utils.js
@@ -6,7 +6,9 @@ export const sendToken = (res, user, message, statusCode=200)=>{
         expires: new Date(Date.now() + 1*24*60*60*1000 ),
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite:"None",    }
+        sameSite:"None",
+        path: '/'
+    }
         res.status(statusCode).cookie("token", token, options).json({
             success:true,
             message,
